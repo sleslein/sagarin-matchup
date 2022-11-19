c682981ce2b73b532c1c1008e0e311b5fc8a03c9
Shea Leslein <sleslein@users.noreply.github.com>
2022-11-19 07:38:24 -0600

    redo api.js path

--- website/edge-function/api.js
+++ website/edge-function/api.js
@@ -1,4 +1,4 @@
-import { main } from 'app/main.ts'
+import { main } from '../../app/main.ts'
 export default async function(request) {
     const week = new URL(request.url).searchParams.get("week");
     const predictions = await main({ week: parseInt(week), verbose: false, log:false });
