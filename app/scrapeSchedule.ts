a8c10d040e73341f0fb50a2862f5339afd3ab60e
Shea Leslein <sleslein@users.noreply.github.com>
2022-11-20 16:11:42 -0600

    upgrade cheerio

--- app/scrapeSchedule.ts
+++ app/scrapeSchedule.ts
@@ -1,4 +1,4 @@
-import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+import { cheerio } from "https://deno.land/x/cheerio@1.0.7/mod.ts";
 import { Game, TeamSchedule, WeeklySchedule } from "./types/index.ts";
 
 async function fetchHtml(): Promise<string> {
