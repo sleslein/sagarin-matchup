a5d7a0223d41b69146f697f4097e3ba0a41897a5
Shea Leslein <sleslein@users.noreply.github.com>
2022-11-30 06:28:50 -0600

    added loggin

--- website/edge-function/api.js
+++ website/edge-function/api.js
@@ -1,7 +1,7 @@
 import { main } from '../../app/main.ts'
 export default async function(request) {
     const week = new URL(request.url).searchParams.get("week");
-    const predictions = await main({ week: parseInt(week), verbose: false, log:false });
+    const predictions = await main({ week: parseInt(week), verbose: true, log:false });
     return new Response(JSON.stringify(predictions));
 } 
 
